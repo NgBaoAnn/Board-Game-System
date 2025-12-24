b81c694de547a2f867a8829cc5122537450e4a73
23120205 <ngogiaan11111@gmail.com>
2025-12-24 17:33:33 +0700

    swagger docs

--- backend/src/routes/index.route.js
+++ backend/src/routes/index.route.js
@@ -1,7 +1,9 @@
 const authRoute = require("./auth.route");
 const userRoute = require("./user.route");
+const swaggerRoute = require("./swagger.route");
 
 const initRoute = (app) => {
+  app.use("/docs", swaggerRoute);
   app.use("/api/auth", authRoute);
   app.use("/api", userRoute);
 };
