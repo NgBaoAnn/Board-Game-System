a770847bf9f47d478c72a91bcef9d8f2ca4d97e2
23120207 <baoan1real1@gmail.com>
2025-12-27 20:50:34 +0700

    add seed data for scores with various game types

--- backend/src/databases/seeds/01_seed_roles.js
+++ backend/src/databases/seeds/01_seed_roles.js
@@ -2,6 +2,10 @@
  * @param {import("knex").Knex} knex
  */
 exports.seed = async function (knex) {
+  // Delete users first to avoid foreign key constraint violation
+  await knex("users").del();
+  
+  // Then delete roles
   await knex("roles").whereIn("id", [1, 2]).del();
 
   await knex("roles").insert([
