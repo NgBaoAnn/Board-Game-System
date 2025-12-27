53806148ec2fde981e31bad5d8522e1f29e18001
23120226 <daihaha123zz@gmail.com>
2025-12-28 00:36:25 +0700

    feat: add RequireAdmin component for admin route protection and update routing to include it

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,49 +1,54 @@
-import { createBrowserRouter } from 'react-router-dom'
-import ClientLayout from '../components/layout/ClientLayout'
-import AdminLayout from '../components/layout/AdminLayout'
-import HomePage from '../pages/HomePage'
-import LoginPage from '../pages/LoginPage'
-import RegisterPage from '../pages/RegisterPage'
-import NotFoundPage from '../pages/NotFoundPage'
-import AdminUsersPage from '../pages/AdminUsersPage'
-import AdminDashboardPage from '@/pages/AdminDashboardPage'
-import AdminGamesPage from '@/pages/AdminGamesPage'
+import { createBrowserRouter } from "react-router-dom";
+import ClientLayout from "../components/layout/ClientLayout";
+import AdminLayout from "../components/layout/AdminLayout";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import NotFoundPage from "../pages/NotFoundPage";
+import AdminUsersPage from "../pages/AdminUsersPage";
+import AdminDashboardPage from "@/pages/AdminDashboardPage";
+import AdminGamesPage from "@/pages/AdminGamesPage";
+import RequireAdmin from "@/components/common/RequireAdmin";
 
 export const router = createBrowserRouter([
-  {
-    element: <ClientLayout />,
-    children: [
-      {
-        path: '/',
-        element: <HomePage />,
-      },
-      {
-        path: '/login',
-        element: <LoginPage />,
-      },
-      {
-        path: '/register',
-        element: <RegisterPage />,
-      },
-    ],
-    errorElement: <NotFoundPage />,
-  },
-  {
-    path: '/admin',
-    element: <AdminLayout />,
-    children: [
-      {
-        path: '/admin/users',
-        element: <AdminUsersPage />,
-      },
-      {
-        path: '/admin/dashboard',
-        element: <AdminDashboardPage />,
-      },
-      {
-        path: '/admin/games',
-        element: <AdminGamesPage />,
-      }
-    ],
-  },
-])
+    {
+        element: <ClientLayout />,
+        children: [
+            {
+                path: "/",
+                element: <HomePage />,
+            },
+            {
+                path: "/login",
+                element: <LoginPage />,
+            },
+            {
+                path: "/register",
+                element: <RegisterPage />,
+            },
+        ],
+        errorElement: <NotFoundPage />,
+    },
+    {
+        path: "/admin",
+        element: (
+            <RequireAdmin>
+                <AdminLayout />
+            </RequireAdmin>
+        ),
+        children: [
+            {
+                path: "/admin/users",
+                element: <AdminUsersPage />,
+            },
+            {
+                path: "/admin/dashboard",
+                element: <AdminDashboardPage />,
+            },
+            {
+                path: "/admin/games",
+                element: <AdminGamesPage />,
+            },
+        ],
+    },
+]);
