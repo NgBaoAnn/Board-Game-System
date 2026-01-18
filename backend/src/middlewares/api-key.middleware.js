67fad038f2212b303f1aa5dc21df1c7895cb0fb5
23120205 <ngogiaan11111@gmail.com>
2026-01-18 15:03:06 +0700

    deb middleware api key

--- backend/src/middlewares/api-key.middleware.js
+++ backend/src/middlewares/api-key.middleware.js
@@ -1,16 +1,16 @@
 const HTTP_STATUS = require("../constants/http-status");
 
 const apiKeyMiddleware = (req, res, next) => {
-    const apiKey = req.headers["x-api-key"];
-    const validApiKey = process.env.API_KEY;
+  const apiKey = req.headers["x-api-key"];
+  const validApiKey = process.env.API_KEY;
 
-    if (!apiKey || apiKey !== validApiKey) {
-        return res.status(HTTP_STATUS.FORBIDDEN).json({
-            message: "Forbidden: Invalid or missing API Key",
-        });
-    }
+  if (!apiKey || apiKey !== validApiKey) {
+    return res.status(HTTP_STATUS.FORBIDDEN).json({
+      message: "Forbidden: Invalid or missing API Key",
+    });
+  }
 
-    next();
+  next();
 };
 
 module.exports = apiKeyMiddleware;
